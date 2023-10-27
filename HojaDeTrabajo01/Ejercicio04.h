1000dc170532aa2ad393c657d8403748527946b7
JOSE CARLOS DIAZ GUERRERO <jcdiazgu@correo.url.edu.gt>
2023-10-26 23:21:07 -0600

    PruebaFinal2

--- HojaDeTrabajo01/Ejercicio04.h
+++ HojaDeTrabajo01/Ejercicio04.h
@@ -8,8 +8,10 @@ using std::string;
 
 class Ejercicio04
 {
+private:
+	void Verificacion(std::vector<std::string>& result, std::string current, int Incio, int Finall);
+
 public:
 	vector<string> generateParenthesis(int n);
-	void Verificacion(std::vector<std::string>& result, std::string current, int Incio, int Finall);
 };
 
