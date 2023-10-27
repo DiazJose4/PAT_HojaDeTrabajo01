bd49482e78414d6ea9684660f5248a7d380225fb
JOSE CARLOS DIAZ GUERRERO <jcdiazgu@correo.url.edu.gt>
2023-10-26 22:54:43 -0600

    Prueba 2

--- HojaDeTrabajo01/Ejercicio02.cpp
+++ HojaDeTrabajo01/Ejercicio02.cpp
@@ -1,6 +1,21 @@
 #include "Ejercicio02.h"
 
-bool Ejercicio02::isPalindrome(string& s)
+bool Ejercicio02::isPalindrome(string& s) {
+    return Palindromo(s, 0, s.length() - 1);
+}
+
+
+bool Ejercicio02::Palindromo(string& s, int Inicio, int final)
 {
-    return false;
+    if (Inicio >= final)
+    {
+        return true;
+    }
+
+    if (s[Inicio] != s[final]) {
+        return false;
+    }
+   
+
+    return Palindromo(s, Inicio + 1, final -1);
 }
--- HojaDeTrabajo01/Ejercicio02.h
+++ HojaDeTrabajo01/Ejercicio02.h
@@ -8,5 +8,6 @@ class Ejercicio02
 {
 public:
 	bool isPalindrome(string &s);
+	bool Palindromo(string& s, int Inicio, int final);
 };
 
