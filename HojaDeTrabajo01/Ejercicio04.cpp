1b2e5bd8aa90a2b85dfc25ac1bd25291be02c09c
JOSE CARLOS DIAZ GUERRERO <jcdiazgu@correo.url.edu.gt>
2023-10-26 23:30:42 -0600

    F

--- HojaDeTrabajo01/Ejercicio04.cpp
+++ HojaDeTrabajo01/Ejercicio04.cpp
@@ -1,23 +1,42 @@
 #include "Ejercicio04.h"
 
-void Verificacion(std::vector<std::string>& result, std::string current, int Incio, int Finall) {
-    if (Incio == 0 && Finall== 0) {
-        result.push_back(current);
+//void Verificacion(std::vector<std::string>& result, std::string current, int Incio, int Finall) {
+    //if (Incio == 0 && Finall== 0) {
+    //    result.push_back(current);
+  //      return;
+//    }
+
+    //if (Incio > 0) {
+      //  Verificacion(result, current + '(', Incio - 1, Finall);
+    //}
+
+    //if (Finall > Incio) {
+    //    Verificacion(result, current + ')', Incio, Finall- 1);
+  //  }
+//}
+
+void generate(int n, int openCount, int closedCount, string sb, vector<string>& results) {
+    if (openCount == n && closedCount == n) {
+        results.push_back(sb);
         return;
     }
 
-    if (Incio > 0) {
-        Verificacion(result, current + '(', Incio - 1, Finall);
+    if (openCount < n) {
+        sb.append("(");
+        generate(n, openCount + 1, closedCount, sb, results);
+        sb.pop_back();
     }
 
-    if (Finall > Incio) {
-        Verificacion(result, current + ')', Incio, Finall- 1);
+    if (closedCount < openCount) {
+        sb.append(")");
+        generate(n, openCount, closedCount + 1, sb, results);
+        sb.pop_back();
     }
 }
 
 vector<string> Ejercicio04::generateParenthesis(int n)
 {
-    std::vector<std::string> Ordenamiento;
-    Verificacion(Ordenamiento, " ", n, n);
-    return Ordenamiento;
+    vector<string> Ordenamienot;
+    generate(n, 0, 0, "", Ordenamienot);
+    return Ordenamienot;
 }
--- HojaDeTrabajo01/Ejercicio04.h
+++ HojaDeTrabajo01/Ejercicio04.h
@@ -8,9 +8,6 @@ using std::string;
 
 class Ejercicio04
 {
-private:
-	void Verificacion(std::vector<std::string>& result, std::string current, int Incio, int Finall);
-
 public:
 	vector<string> generateParenthesis(int n);
 };
